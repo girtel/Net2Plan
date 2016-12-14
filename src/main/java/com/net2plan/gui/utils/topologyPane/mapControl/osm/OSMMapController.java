c03fcf5ff4922c215b2bfb13018805f12c3d42af
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2016-12-14 13:02:30 +0100

    Remove zoom limits

--- src/main/java/com/net2plan/gui/utils/topologyPane/mapControl/osm/OSMMapController.java
+++ src/main/java/com/net2plan/gui/utils/topologyPane/mapControl/osm/OSMMapController.java
@@ -366,12 +366,7 @@ public class OSMMapController
     {
         if (isMapActivated())
         {
-            final int maximumZoomLevel = mapViewer.getTileFactory().getInfo().getMaximumZoomLevel() - 1;
-
-            if (!(mapViewer.getZoom() == maximumZoomLevel))
-            {
-                mapViewer.setZoom(mapViewer.getZoom() + 1);
-            }
+            mapViewer.setZoom(mapViewer.getZoom() + 1);
 
             // Align the topology to the newly change OSM map.
             alignZoomJUNGToOSMMap();
