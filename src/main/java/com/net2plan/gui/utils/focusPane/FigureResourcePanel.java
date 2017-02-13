cfe5a4c0eafce246db79cf4b3226b6c6e8feef67
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2017-02-13 13:04:47 +0100

    Same with resources

--- src/main/java/com/net2plan/gui/utils/focusPane/FigureResourcePanel.java
+++ src/main/java/com/net2plan/gui/utils/focusPane/FigureResourcePanel.java
@@ -9,6 +9,7 @@ import java.awt.Graphics2D;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import com.net2plan.gui.utils.IVisualizationCallback;
@@ -97,19 +98,26 @@ public class FigureResourcePanel extends FigureSequencePanel
 
     	/* Now the base resource and the links from the resource */
     	xPositionResource = initialXTitle;
-    	for (Resource baseResource : resource.getBaseResources())
+        final Iterator<Resource> iterator = resource.getBaseResources().iterator();
+        while (iterator.hasNext())
     	{
+    	    final Resource baseResource = iterator.next();
     		/* Add the gn */
         	final Point dnTopLeftPosition = new Point (xPositionResource , initialTopYOfBaseResource);
         	final DrawNode dn = new DrawNode(baseResource , maxHeightOrSizeIcon , resource.getCapacityOccupiedInBaseResource(baseResource));
         	this.drawnNodes.add(dn);
-        	DrawNode.addNodeToGraphics(g2d , dn , dnTopLeftPosition , fontMetrics , regularInterlineSpacePixels , null);
+        	final Dimension windowSize = DrawNode.addNodeToGraphics(g2d , dn , dnTopLeftPosition , fontMetrics , regularInterlineSpacePixels , null);
         	xPositionResource += xSeparationDnCenters;
 
     		/* Add the glink */
 			final DrawLine dlNoURL = new DrawLine (dnResource , dn , dnResource.posSouth() , dn.posNorth());
 			DrawLine.addLineToGraphics(g2d , dlNoURL , fontMetrics , regularInterlineSpacePixels);
 			drawnLines.add(dlNoURL);
+
+			if (!iterator.hasNext())
+            {
+                this.preferredSize = new Dimension(windowSize.width + XYMARGIN, windowSize.height + XYMARGIN);
+            }
     	}
     }
 }
