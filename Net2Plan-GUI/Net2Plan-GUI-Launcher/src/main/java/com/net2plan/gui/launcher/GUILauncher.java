80b07a38869a6513f1d69f3b021836b18b2550cf
Jorge San Emeterio <jorge.s.emeterio@gmail.com>
2017-02-20 09:53:10 +0100

    Working on the GUI Launcher

--- Net2Plan-GUI/Net2Plan-GUI-Launcher/src/main/java/com/net2plan/gui/launcher/GUILauncher.java
+++ Net2Plan-GUI/Net2Plan-GUI-Launcher/src/main/java/com/net2plan/gui/launcher/GUILauncher.java
@@ -1,6 +1,9 @@
 package com.net2plan.gui.launcher;
 
 import com.net2plan.gui.GUINet2Plan;
+import com.net2plan.gui.plugins.GUINetworkDesign;
+import com.net2plan.gui.plugins.GUITrafficDesign;
+import com.net2plan.internal.plugins.IGUIModule;
 import com.net2plan.internal.plugins.PluginSystem;
 
 public class GUILauncher
@@ -8,8 +11,8 @@ public class GUILauncher
 	public static void main(String[] args)
 	{
 		GUINet2Plan.main(args);
-		//PluginSystem.addPlugin(IGUIModule.class, GUINetworkDesign.class);
-		//PluginSystem.addPlugin(IGUIModule.class, GUITrafficDesign.class);
+		PluginSystem.addPlugin(IGUIModule.class, GUINetworkDesign.class);
+		PluginSystem.addPlugin(IGUIModule.class, GUITrafficDesign.class);
 		PluginSystem.loadExternalPlugins();
 		GUINet2Plan.refreshMenu();
 	}
