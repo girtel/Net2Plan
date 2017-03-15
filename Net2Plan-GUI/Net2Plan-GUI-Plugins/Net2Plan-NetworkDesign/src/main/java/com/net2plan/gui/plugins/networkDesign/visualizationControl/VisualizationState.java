5092fb688968382a27bc4a4106359106f6539844
Jorge SE <jorge.s.emeterio@gmail.com>
2017-03-15 10:56:25 +0100

    Adapting visualization state

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/visualizationControl/VisualizationState.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/visualizationControl/VisualizationState.java
@@ -11,6 +11,7 @@ import com.net2plan.utils.Pair;
 import com.net2plan.utils.Triple;
 import edu.uci.ics.jung.visualization.FourPassImageShaper;
 import org.apache.commons.collections15.BidiMap;
+import org.apache.commons.collections15.MapUtils;
 import org.apache.commons.collections15.bidimap.DualHashBidiMap;
 
 import javax.imageio.ImageIO;
@@ -403,7 +404,7 @@ public class VisualizationState
         this.cache_mapNode2ListVerticallyStackedGUINodes = new HashMap<>();
         for (int layerVisualizationOrderIncludingNonVisible = 0; layerVisualizationOrderIncludingNonVisible < this.getNetPlan().getNumberOfLayers(); layerVisualizationOrderIncludingNonVisible++)
         {
-            final NetworkLayer layer = visualizationSnapshot.getMapCanvasLayerVisualizationOrder().inverseBidiMap().get(layerVisualizationOrderIncludingNonVisible);
+            final NetworkLayer layer = MapUtils.invertMap(visualizationSnapshot.getMapCanvasLayerVisualizationOrder()).get(layerVisualizationOrderIncludingNonVisible);
             if (isLayerVisibleInCanvas(layer))
                 cache_mapCanvasVisibleLayer2VisualizationOrderRemovingNonVisible.put(layer, cache_mapCanvasVisibleLayer2VisualizationOrderRemovingNonVisible.size());
         }
@@ -673,7 +674,7 @@ public class VisualizationState
         if (visualizationOrder < 0) throw new RuntimeException("");
         if (visualizationOrder >= this.getNetPlan().getNumberOfLayers())
             throw new RuntimeException("");
-        return visualizationSnapshot.getMapCanvasLayerVisualizationOrder().inverseBidiMap().get(visualizationOrder);
+        return MapUtils.invertMap(visualizationSnapshot.getMapCanvasLayerVisualizationOrder()).get(visualizationOrder);
     }
 
     public int getCanvasVisualizationOrderRemovingNonVisible(NetworkLayer layer)
@@ -1292,7 +1293,7 @@ public class VisualizationState
 
     public List<NetworkLayer> getCanvasLayersInVisualizationOrder(boolean includeNonVisible)
     {
-        BidiMap<Integer, NetworkLayer> map = includeNonVisible ? visualizationSnapshot.getMapCanvasLayerVisualizationOrder().inverseBidiMap() : cache_mapCanvasVisibleLayer2VisualizationOrderRemovingNonVisible.inverseBidiMap();
+        BidiMap<Integer, NetworkLayer> map = includeNonVisible ? new DualHashBidiMap<>(MapUtils.invertMap(visualizationSnapshot.getMapCanvasLayerVisualizationOrder())) : cache_mapCanvasVisibleLayer2VisualizationOrderRemovingNonVisible.inverseBidiMap();
         List<NetworkLayer> res = new ArrayList<>();
         for (int vIndex = 0; vIndex < this.getNetPlan().getNumberOfLayers(); vIndex++)
             res.add(map.get(vIndex));
