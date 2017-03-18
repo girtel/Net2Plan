21682a056921610fe7ea3acd9facffd5bd28cf93
Jorge SE <jorge.s.emeterio@gmail.com>
2017-03-18 19:04:09 +0100

    Skeleton for mode

--- Net2Plan-GUI/Net2Plan-GUI-Launcher/src/main/java/com/net2plan/gui/launcher/GUIPluginLauncher.java
+++ Net2Plan-GUI/Net2Plan-GUI-Launcher/src/main/java/com/net2plan/gui/launcher/GUIPluginLauncher.java
@@ -14,6 +14,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.util.Map;
 
 /**
  * Created by Jorge San Emeterio on 17/03/17.
@@ -35,10 +36,14 @@ public class GUIPluginLauncher
             final Option pluginOption = new Option("t", "tool", true, "Tool/Plugin to be launched");
             pluginOption.setRequired(true);
 
+            final Option modeParamOption = new Option("m", "mode", true, "Tool/Plugin launch mode");
+            pluginOption.setRequired(false);
+
             final Option pluginParamOption = new Option("p", "param", true, "Tool/Plugin specific parameters");
             pluginParamOption.setRequired(false);
 
             options.addOption(pluginOption);
+            options.addOption(modeParamOption);
             options.addOption(pluginParamOption);
 
             parser = new DefaultParser();
@@ -51,25 +56,54 @@ public class GUIPluginLauncher
             ClassLoader cl = GUIPluginLauncher.class.getClassLoader();
             final ImmutableSet<ClassPath.ClassInfo> classesInNet2Plan = ClassPath.from(cl).getTopLevelClasses("com.net2plan.gui.plugins");
 
-            Object plugin = null;
+            IGUIModule plugin = null;
             for (ClassPath.ClassInfo classInfo : classesInNet2Plan)
             {
                 if (classInfo.getSimpleName().equalsIgnoreCase(inputPlugin))
                 {
-                    plugin = Class.forName(classInfo.toString()).newInstance();
+                    final Object instance = Class.forName(classInfo.toString()).newInstance();
+                    if(instance instanceof IGUIModule) plugin = (IGUIModule) instance;
                     break;
                 }
             }
 
-            if (plugin == null || !(plugin instanceof IGUIModule)) throw new RuntimeException();
+            // Plugin not found
+            if (plugin == null) throw new ParserException("Plugin: " + inputPlugin + " not found.");
+            currentPlugin = plugin;
 
-            currentPlugin = (IGUIModule) plugin;
-
+            // Run Net2Plan
             GUINet2Plan.main(args);
             PluginSystem.addPlugin(IGUIModule.class, currentPlugin.getClass());
             GUINet2Plan.refreshMenu();
 
             runPlugin();
+
+            // Parse mode and params
+            int mode = 1;
+
+            // Looking for plugin wrapper
+            final ImmutableSet<ClassPath.ClassInfo> wrappers = ClassPath.from(cl).getTopLevelClasses("com.net2plan.gui.launcher.wrapper");
+
+            Object wrapper = null;
+            for (ClassPath.ClassInfo classInfo : wrappers)
+            {
+                if (classInfo.getSimpleName().equalsIgnoreCase(inputPlugin))
+                {
+                    wrapper = Class.forName(classInfo.toString()).newInstance();
+                    break;
+                }
+            }
+
+            if (wrapper != null && cmd.hasOption("mode"))
+            {
+                mode = Integer.parseInt(cmd.getOptionValue("mode"));
+
+                if (cmd.hasOption("param"))
+                {
+
+                }
+            }
+
         } catch (ParseException e)
         {
             System.out.println(e.getMessage());
@@ -100,19 +134,11 @@ public class GUIPluginLauncher
 
         robot.keyRelease(pluginKeyStroke.getKeyCode());
         robot.keyRelease(keyModifier);
+    }
 
-        // Tool specific actions
-
-//        robot.keyPress(KeyEvent.VK_ALT);
-//        robot.keyPress(KeyEvent.VK_F4);
-//        robot.keyRelease(KeyEvent.VK_F4);
-//        robot.keyRelease(KeyEvent.VK_ALT);
-//
-//        robot.keyPress(KeyEvent.VK_CONTROL);
-//        robot.keyPress(KeyEvent.VK_O);
-//
-//        robot.keyRelease(KeyEvent.VK_O);
-//        robot.keyRelease(KeyEvent.VK_CONTROL);
+    private static Map<String, String> getParameters()
+    {
+        return null;
     }
 
     private static int getKeyModifier(final KeyStroke keyStroke)
---
+++ Net2Plan-GUI/Net2Plan-GUI-Launcher/src/main/java/com/net2plan/gui/launcher/wrapper/GUINetworkDesignWrapper.java
@@ -0,0 +1,17 @@
+package com.net2plan.gui.launcher.wrapper;
+
+/**
+ * Created by punks on 18/03/2017.
+ */
+public class GUINetworkDesignWrapper
+{
+    public static void launchMode(final int mode)
+    {
+        switch (mode)
+        {
+            case 1:
+            default:
+                return;
+        }
+    }
+}
