5fc311d6882b9c5953fd9d4b3d1028724dddf71d
Jorge SE <jorge.s.emeterio@gmail.com>
2017-04-04 10:25:07 +0200

    Added node

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/focusPane/FigureForwardingRuleSequencePanel.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/focusPane/FigureForwardingRuleSequencePanel.java
@@ -1,26 +1,50 @@
+/*
+ * ******************************************************************************
+ *  * Copyright (c) 2017 Pablo Pavon-Marino.
+ *  * All rights reserved. This program and the accompanying materials
+ *  * are made available under the terms of the GNU Lesser Public License v3.0
+ *  * which accompanies this distribution, and is available at
+ *  * http://www.gnu.org/licenses/lgpl.html
+ *  *
+ *  * Contributors:
+ *  *     Pablo Pavon-Marino - Jose-Luis Izquierdo-Zaragoza, up to version 0.3.1
+ *  *     Pablo Pavon-Marino - from version 0.4.0 onwards
+ *  *     Pablo Pavon Marino - Jorge San Emeterio Villalain, from version 0.4.1 onwards
+ *  *****************************************************************************
+ */
+
 package com.net2plan.gui.plugins.networkDesign.focusPane;
 
 import com.net2plan.gui.plugins.GUINetworkDesign;
+import com.net2plan.interfaces.networkDesign.Demand;
+import com.net2plan.interfaces.networkDesign.Link;
 import com.net2plan.interfaces.networkDesign.NetworkLayer;
-import com.net2plan.interfaces.networkDesign.Node;
+import com.net2plan.utils.Pair;
 
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
-public class FigureNodeSequencePanel extends FigureSequencePanel
+/**
+ * @author Jorge San Emeterio Villalain
+ * @date 4/04/17
+ */
+public class FigureForwardingRuleSequencePanel extends FigureSequencePanel
 {
-    private DrawNode dn;
+    private Pair<Demand, Link> forwardingRule;
+    private NetworkLayer networkLayer;
     private List<String> generalMessage;
-    private Node node;
-    private NetworkLayer layer;
     private Dimension preferredSize;
 
-    public FigureNodeSequencePanel(GUINetworkDesign callback, Node node, NetworkLayer layer, String... titleMessage)
+    public FigureForwardingRuleSequencePanel(GUINetworkDesign callback, Pair<Demand, Link> forwardingRule, NetworkLayer networkLayer, String... titleMessage)
     {
         super(callback);
-        this.layer = layer;
-        this.node = node;
+
+        assert forwardingRule != null;
+        assert networkLayer != null;
+
+        this.forwardingRule = forwardingRule;
+        this.networkLayer = networkLayer;
         this.generalMessage = Arrays.asList(titleMessage);
         this.preferredSize = null;
     }
@@ -60,11 +84,9 @@ public class FigureNodeSequencePanel extends FigureSequencePanel
         final int topCoordinateLineNodes = currentXYStartOfText + (maxNumberOfTagsPerNodeNorResource * regularInterlineSpacePixels);
         final Point initialDnTopLeftPosition = new Point(maxHeightOrSizeIcon, topCoordinateLineNodes);
 
-    	/* Initial dn */
-        dn = new DrawNode(node, layer, maxHeightOrSizeIcon);
+        DrawNode dn = new DrawNode(forwardingRule.getSecond().getOriginNode(), networkLayer, maxHeightOrSizeIcon);
         final Dimension windowDimension = DrawNode.addNodeToGraphics(g2d , dn , initialDnTopLeftPosition , fontMetrics , regularInterlineSpacePixels , null);
         drawnNodes.add(dn);
         preferredSize = new Dimension (windowDimension.width + XYMARGIN , windowDimension.height + XYMARGIN);
     }
 }
-
