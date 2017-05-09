192b665d41b72b844aea1d0e47a3822f5b259d45
Jorge SE <jorge.s.emeterio@gmail.com>
2017-05-09 11:47:50 +0200

    Working on bugs

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/AggregationUtils.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/AggregationUtils.java
@@ -10,10 +10,19 @@ public class AggregationUtils
 {
     public static void updateRowSum(double[] aggreg, int index, Object val)
     {
-        if (val instanceof Double)
-            aggreg[index] += (Double) val;
-        else
+        if (val instanceof String || val instanceof Number)
+        {
+            try
+            {
+                aggreg[index] += Double.parseDouble(val.toString());
+            } catch (NumberFormatException e)
+            {
+                ErrorHandling.log("Wrong number format for: " + val.toString());
+            }
+        } else
+        {
             ErrorHandling.log("Could not aggregate value of type: " + val.getClass());
+        }
     }
 
     public static void updateRowMax(double[] aggreg, int index, Object val)
