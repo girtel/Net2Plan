58ab827b6beaae1ad516a006eeb50568437395ab
Jorge SE <jorge.s.emeterio@gmail.com>
2017-05-09 11:19:14 +0200

    Finish SRG

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/specificTables/AdvancedJTable_resource.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/specificTables/AdvancedJTable_resource.java
@@ -7,6 +7,8 @@ import com.net2plan.gui.plugins.networkDesign.CellRenderers;
 import com.net2plan.gui.plugins.networkDesign.ElementSelection;
 import com.net2plan.gui.plugins.networkDesign.interfaces.ITableRowFilter;
 import com.net2plan.gui.plugins.networkDesign.viewEditTopolTables.controlTables.AdvancedJTable_networkElement;
+import com.net2plan.gui.plugins.networkDesign.viewEditTopolTables.controlTables.AggregationUtils;
+import com.net2plan.gui.plugins.networkDesign.viewEditTopolTables.controlTables.LastRowAggregatedValue;
 import com.net2plan.gui.utils.*;
 import com.net2plan.interfaces.networkDesign.*;
 import com.net2plan.internal.Constants;
@@ -14,7 +16,6 @@ import com.net2plan.internal.Constants.NetworkElementType;
 import com.net2plan.internal.ErrorHandling;
 import com.net2plan.utils.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
-
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -73,6 +74,8 @@ public class AdvancedJTable_resource extends AdvancedJTable_networkElement
     {
         final List<Object[]> allResourceData = new LinkedList<Object[]>();
         final List<Resource> rowVisibleResources = getVisibleElementsInTable();
+        final double[] dataAggregator = new double[netPlanViewTableHeader.length];
+
         for (Resource res : rowVisibleResources)
         {
             Object[] resData = new Object[netPlanViewTableHeader.length + attributesTitles.size()];
@@ -99,25 +102,26 @@ public class AdvancedJTable_resource extends AdvancedJTable_networkElement
                 }
             }
 
-            allResourceData.add(resData);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_CAPACITY, resData[COLUMN_CAPACITY]);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_OCCUPIEDCAPACITY, resData[COLUMN_OCCUPIEDCAPACITY]);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_TRAVERSINGROUTES, resData[COLUMN_TRAVERSINGROUTES]);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_PROCESSINGTIME, resData[COLUMN_PROCESSINGTIME]);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_UPPERRESOURCES, resData[COLUMN_UPPERRESOURCES]);
+            AggregationUtils.updateRowSum(dataAggregator, COLUMN_BASERESOURCES, resData[COLUMN_BASERESOURCES]);
 
+            allResourceData.add(resData);
         }
 
         /* Add the aggregation row with the aggregated statistics */
-        final double aggCapacity = rowVisibleResources.stream().mapToDouble(e -> e.getCapacity()).sum();
-        final double aggOccupiedCapacity = rowVisibleResources.stream().mapToDouble(e -> e.getOccupiedCapacity()).sum();
-        final int aggTravSCs = rowVisibleResources.stream().mapToInt(e -> e.getTraversingRoutes().size()).sum();
-        final double aggMaxProcTime = rowVisibleResources.stream().mapToDouble(e -> e.getProcessingTimeToTraversingTrafficInMs()).max().orElse(0);
         final LastRowAggregatedValue[] aggregatedData = new LastRowAggregatedValue[netPlanViewTableHeader.length + attributesTitles.size()];
         Arrays.fill(aggregatedData, new LastRowAggregatedValue());
-        aggregatedData[COLUMN_CAPACITY] = new LastRowAggregatedValue(aggCapacity); // sum 
-        aggregatedData[COLUMN_OCCUPIEDCAPACITY] = new LastRowAggregatedValue(aggOccupiedCapacity); // sum 
-        aggregatedData[COLUMN_TRAVERSINGROUTES] = new LastRowAggregatedValue(aggTravSCs); // sum 
-        aggregatedData[COLUMN_PROCESSINGTIME] = new LastRowAggregatedValue(aggMaxProcTime); // sum 
+        aggregatedData[COLUMN_CAPACITY] = new LastRowAggregatedValue(dataAggregator[COLUMN_CAPACITY]);
+        aggregatedData[COLUMN_OCCUPIEDCAPACITY] = new LastRowAggregatedValue(dataAggregator[COLUMN_OCCUPIEDCAPACITY]);
+        aggregatedData[COLUMN_TRAVERSINGROUTES] = new LastRowAggregatedValue(dataAggregator[COLUMN_TRAVERSINGROUTES]);
+        aggregatedData[COLUMN_PROCESSINGTIME] = new LastRowAggregatedValue(dataAggregator[COLUMN_PROCESSINGTIME]);
+        aggregatedData[COLUMN_UPPERRESOURCES] = new LastRowAggregatedValue(dataAggregator[COLUMN_UPPERRESOURCES]);
+        aggregatedData[COLUMN_BASERESOURCES] = new LastRowAggregatedValue(dataAggregator[COLUMN_BASERESOURCES]);
 
-        // JORGE!!! HACER LO MISMO PARA OLUMN_UPPERRESOURCES Y COLUMN_BASERESOURCES como // sum
-
-        
         allResourceData.add(aggregatedData);
 
         return allResourceData;
