6f1806da91b194c481aa7ba65fd4ec8d8f98440f
Jorge SE <jorge.s.emeterio@gmail.com>
2017-05-10 09:45:22 +0200

    Added JUnitParams

---
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/TableSearcher.java
@@ -0,0 +1,9 @@
+package com.net2plan.gui.plugins.networkDesign.viewEditTopolTables.controlTables;
+
+/**
+ * @author Jorge San Emeterio
+ * @date 10/05/17
+ */
+public class TableSearcher
+{
+}
---
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/test/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/TableSearcherTest.java
@@ -0,0 +1,12 @@
+package com.net2plan.gui.plugins.networkDesign.viewEditTopolTables.controlTables;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author Jorge San Emeterio
+ * @date 10/05/17
+ */
+@RunWith(JUnitPara)
+public class TableSearcherTest
+{
+}
