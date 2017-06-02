3430f42622c12b7eeefa90d509d89aea74fb573f
CFSNM <cesarfsannicolasmartinez@gmail.com>
2017-06-02 09:55:34 +0200

    Modidying Travis for only desploying when a tag has been manually created

--- Net2Plan-CI/src/scripts/get_version.sh
+++ Net2Plan-CI/src/scripts/get_version.sh
@@ -7,7 +7,3 @@ MVN_VERSION=$(mvn -q \
     org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
 echo "$MVN_VERSION"
 
-git config --global user.email $EMAIL
-git config --global user.name $USER
-
-git tag -a $MVN_VERSION -m '$MVN_VERSION tag'
