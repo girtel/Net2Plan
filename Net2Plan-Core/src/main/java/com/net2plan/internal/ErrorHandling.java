862f43017a8a382cf572e4d1b3bf1d3c1e2d5d05
Jorge SE <jorge.s.emeterio@gmail.com>
2018-01-10 13:16:29 +0100

    Allowing error handling to be reconfigured from a more subtle approach

--- Net2Plan-Core/src/main/java/com/net2plan/internal/ErrorHandling.java
+++ Net2Plan-Core/src/main/java/com/net2plan/internal/ErrorHandling.java
@@ -10,14 +10,6 @@
  *******************************************************************************/
 
 
-
-
- 
-
-
-
-
-
 package com.net2plan.internal;
 
 import com.net2plan.internal.Constants.UserInterface;
@@ -26,7 +18,6 @@ import com.net2plan.utils.StringUtils;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
@@ -34,7 +25,7 @@ import java.util.*;
 
 /**
  * Class handling errors within Net2Plan.
- * 
+ *
  * @author Pablo Pavon-Marino, Jose-Luis Izquierdo-Zaragoza
  * @since 0.2.0
  */
@@ -45,39 +36,42 @@ public class ErrorHandling
 	 *
 	 * @since 0.2.0
 	 */
-	public final static OutputStream STREAM;
-	
-	private final static JFrame consoleDialog;
-	private final static JTextArea log;
-	private final static String NEWLINE = StringUtils.getLineSeparator();
+	public static OutputStream STREAM;
 	public static boolean DEBUG = false;
 
-	static
-	{
-		STREAM = new OutputStream()
-		{
-			@Override
-			public void write(int b) throws IOException
-			{
-				log.append(new String(new byte[] { (byte) b }, StandardCharsets.UTF_8));
-			}
-		};
+	public static JFrame consoleDialog;
+	public static JTextArea log;
+	public static String NEWLINE;
 
-		if (SystemUtils.getUserInterface() == UserInterface.GUI)
+	public static void configure()
+	{
+		if (STREAM == null)
+		{
+			STREAM = new OutputStream()
+			{
+				@Override
+				public void write(int b)
+				{
+					log.append(new String(new byte[]{(byte) b}, StandardCharsets.UTF_8));
+				}
+			};
+		}
+
+		if (NEWLINE == null)
+		{
+			NEWLINE = StringUtils.getLineSeparator();
+		}
+
+		UserInterface ui = SystemUtils.getUserInterface();
+
+		if (ui != null && ui == UserInterface.GUI)
 		{
 			consoleDialog = new JFrame();
 			consoleDialog.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
 			Container contentPane = consoleDialog.getContentPane();
 			if (contentPane instanceof JComponent)
 			{
-				((JComponent) contentPane).registerKeyboardAction(new ActionListener()
-				{
-					@Override
-					public void actionPerformed(ActionEvent e)
-					{
-						consoleDialog.setVisible(false);
-					}
-				}, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+				((JComponent) contentPane).registerKeyboardAction(e -> consoleDialog.setVisible(false), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 			}
 			consoleDialog.setSize(new Dimension(500, 200));
 			consoleDialog.setLocationRelativeTo(null);
@@ -91,18 +85,11 @@ public class ErrorHandling
 
 			JButton btn_reset = new JButton("Reset");
 			btn_reset.setToolTipText("Clear the console");
-			btn_reset.addActionListener(new ActionListener()
-			{
-				@Override
-				public void actionPerformed(ActionEvent ae)
-				{
-					log.setText("");
-				}
-			});
+			btn_reset.addActionListener(ae -> log.setText(""));
 
 			JPanel bottomPanel = new JPanel(new FlowLayout());
 			bottomPanel.add(btn_reset);
-			consoleDialog.add(bottomPanel, BorderLayout.SOUTH);			
+			consoleDialog.add(bottomPanel, BorderLayout.SOUTH);
 		}
 		else
 		{
@@ -113,7 +100,7 @@ public class ErrorHandling
 
 	/**
 	 * Adds the output of a {@code Throwable} to the console.
-	 * 
+	 *
 	 * @param throwable {@code Throwable}
 	 * @since 0.2.0
 	 */
@@ -123,74 +110,89 @@ public class ErrorHandling
 	}
 
 	/**
-	 * Adds the output of a {@code Throwable} to the console. In case that debug 
-	 * mode is disabled, the stack trace will stop once a stack element refers to 
+	 * Adds the output of a {@code Throwable} to the console. In case that debug
+	 * mode is disabled, the stack trace will stop once a stack element refers to
 	 * the given class.
-	 * 
+	 *
 	 * @param throwable {@code Throwable}
-	 * @param _class Class that should be omitted from the output
+	 * @param _class    Class that should be omitted from the output
 	 * @since 0.2.0
 	 */
 	public static void addErrorOrException(Throwable throwable, final Class _class)
 	{
 		Set<Class> aux = new LinkedHashSet<Class>();
 		aux.add(_class);
-		
+
 		addErrorOrException(throwable, aux);
 	}
 
 	/**
-	 * Adds the output of a {@code Throwable} to the console. In case that debug 
-	 * mode is disabled, the stack trace will stop once a stack element refers to 
+	 * Adds the output of a {@code Throwable} to the console. In case that debug
+	 * mode is disabled, the stack trace will stop once a stack element refers to
 	 * one of the given classes.
-	 * 
+	 *
 	 * @param throwable {@code Throwable}
-	 * @param _classes Classes that should be omitted from the output (null means 'empty')
+	 * @param _classes  Classes that should be omitted from the output (null means 'empty')
 	 * @since 0.2.0
 	 */
 	public static void addErrorOrException(Throwable throwable, Collection<Class> _classes)
 	{
-		while(true)
+		while (true)
 		{
 			Throwable aux = throwable.getCause();
-			if (aux == null) break;
-			
+			if (aux == null)
+			{
+				break;
+			}
+
 			throwable = aux;
 		}
 
 		StringBuilder text = new StringBuilder();
 		text.append(throwable.toString()).append(NEWLINE);
 		StackTraceElement[] stack = throwable.getStackTrace();
-		
+
 		Set<String> classNames = new LinkedHashSet<String>();
 		Iterator<Class> it = _classes.iterator();
-		while (it.hasNext()) classNames.add(it.next().getName());
+		while (it.hasNext())
+		{
+			classNames.add(it.next().getName());
+		}
 
 		for (StackTraceElement line : stack)
 		{
 			String className = line.getClassName();
 			int pos = className.indexOf('$');
-			if (pos != -1) className = className.substring(0, pos);
+			if (pos != -1)
+			{
+				className = className.substring(0, pos);
+			}
 
-			if (!DEBUG && classNames.contains(className)) break;
+			if (!DEBUG && classNames.contains(className))
+			{
+				break;
+			}
 			text.append(line.toString()).append(NEWLINE);
 		}
-		
+
 		addText(text.toString());
 	}
 
 	private static void addText(String text)
 	{
 		text = new Date().toString() + NEWLINE + NEWLINE + text;
-		
-		switch(SystemUtils.getUserInterface())
+
+		switch (SystemUtils.getUserInterface())
 		{
 			case CLI:
 				System.out.println(text);
 				break;
-						
+
 			case GUI:
-				if (!log.getText().isEmpty()) text = NEWLINE + NEWLINE + text;
+				if (!log.getText().isEmpty())
+				{
+					text = NEWLINE + NEWLINE + text;
+				}
 				log.append(text);
 				break;
 		}
@@ -198,7 +200,7 @@ public class ErrorHandling
 
 	/**
 	 * Returns the most internal cause of a {@code Throwable}.
-	 * 
+	 *
 	 * @param e Internal {@code Throwable}
 	 * @return Internal cause of a {@code Throwable}
 	 * @since 0.2.3
@@ -208,10 +210,10 @@ public class ErrorHandling
 		Throwable cause = e.getCause();
 		return cause == null ? e : getInternalThrowable(cause);
 	}
-	
+
 	/**
 	 * Indicates whether debug is enabled or not
-	 * 
+	 *
 	 * @return {@code true} if debug is enabled. Otherwise, {@code false}
 	 * @since 0.3.0
 	 */
@@ -219,10 +221,10 @@ public class ErrorHandling
 	{
 		return DEBUG;
 	}
-	
+
 	/**
 	 * Prints the whole stack trace of a {@code Throwable}.
-	 * 
+	 *
 	 * @param throwable Internal {@code Throwable}
 	 * @since 0.3.0
 	 */
@@ -232,14 +234,17 @@ public class ErrorHandling
 		System.out.println();
 
 		StackTraceElement[] stack = throwable.getStackTrace();
-		for (StackTraceElement line : stack) System.out.println(line);
+		for (StackTraceElement line : stack)
+		{
+			System.out.println(line);
+		}
 	}
 
 	/**
-	 * Enables/disables the debug mode. If debug mode is enabled, the whole stack trace 
-	 * of {@code Throwable} objects will be shown. Otherwise, developer can customize 
+	 * Enables/disables the debug mode. If debug mode is enabled, the whole stack trace
+	 * of {@code Throwable} objects will be shown. Otherwise, developer can customize
 	 * what classes can be omitted from the output.
-	 * 
+	 *
 	 * @param debug Indicates whether the debug mode is enabled ({@code true}) or disabled ({@code false})
 	 * @since 0.2.3
 	 */
@@ -250,12 +255,15 @@ public class ErrorHandling
 
 	/**
 	 * Shows the output console (only for GUI).
-	 * 
+	 *
 	 * @since 0.2.0
 	 */
 	public static void showConsole()
 	{
-		if (!consoleDialog.isVisible()) consoleDialog.setVisible(true);
+		if (!consoleDialog.isVisible())
+		{
+			consoleDialog.setVisible(true);
+		}
 
 		SwingUtilities.invokeLater(new Runnable()
 		{
@@ -297,6 +305,7 @@ public class ErrorHandling
 
 	/**
 	 * Prints a text into the console log in case the debug mode is activated.
+	 *
 	 * @param text Text to print.
 	 */
 	public static void log(String text)
@@ -320,7 +329,7 @@ public class ErrorHandling
 
 	/**
 	 * Shows a popup error indicating that user should check the output console (only for GUI).
-	 * 
+	 *
 	 * @param title Title for the error dialog
 	 * @since 0.2.0
 	 */
@@ -331,9 +340,9 @@ public class ErrorHandling
 
 	/**
 	 * Shows a popup error indicating an error to the user (only for GUI).
-	 * 
+	 *
 	 * @param message Message to be shown
-	 * @param title Title for the error dialog
+	 * @param title   Title for the error dialog
 	 * @since 0.2.0
 	 */
 	public static void showErrorDialog(String message, String title)
@@ -343,9 +352,9 @@ public class ErrorHandling
 
 	/**
 	 * Shows a popup indicating some information to the user (only for GUI).
-	 * 
+	 *
 	 * @param message Message to be shown
-	 * @param title Title for the information dialog
+	 * @param title   Title for the information dialog
 	 * @since 0.2.0
 	 */
 	public static void showInformationDialog(String message, String title)
@@ -356,20 +365,26 @@ public class ErrorHandling
 	private static void showMessage(String message, String title, int type)
 	{
 		boolean wasVisible = consoleDialog.isVisible();
-		if (wasVisible) consoleDialog.setVisible(false);
-		
+		if (wasVisible)
+		{
+			consoleDialog.setVisible(false);
+		}
+
 		Object data = message;
 
 		JOptionPane.showMessageDialog(null, data, title, type);
-		
-		if (wasVisible) consoleDialog.setVisible(true);
+
+		if (wasVisible)
+		{
+			consoleDialog.setVisible(true);
+		}
 	}
 
 	/**
 	 * Shows a popup indicating some message to the user (only for GUI).
-	 * 
+	 *
 	 * @param message Message to be shown
-	 * @param title Title for the message dialog
+	 * @param title   Title for the message dialog
 	 * @since 0.2.0
 	 */
 	public static void showMessageDialog(String message, String title)
@@ -379,9 +394,9 @@ public class ErrorHandling
 
 	/**
 	 * Shows a popup indicating some warning to the user (only for GUI).
-	 * 
+	 *
 	 * @param message Message to be shown
-	 * @param title Title for the warning dialog
+	 * @param title   Title for the warning dialog
 	 * @since 0.2.0
 	 */
 	public static void showWarningDialog(String message, String title)
