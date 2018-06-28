2f6e79428666b40b41cd0ee67896197f14858c02
Javier Lopez Fernandez <javier.lopezf92@gmail.com>
2018-06-28 13:53:15 +0200

    Starting layers panel

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/topologyPane/TopologySideBar.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/topologyPane/TopologySideBar.java
@@ -10,34 +10,52 @@
  *******************************************************************************/
 package com.net2plan.gui.plugins.networkDesign.topologyPane;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
 import com.net2plan.gui.plugins.GUINetworkDesign;
 import com.net2plan.gui.plugins.networkDesign.interfaces.ITopologyCanvas;
 import com.net2plan.gui.plugins.networkDesign.visualizationControl.VisualizationConstants;
 import com.net2plan.gui.plugins.networkDesign.visualizationControl.VisualizationState;
+import com.net2plan.interfaces.networkDesign.NetPlan;
+import com.net2plan.interfaces.networkDesign.NetworkLayer;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import net.miginfocom.swing.MigLayout;
 
-/**
- * @author Jorge San Emeterio
- * @date 25/04/17
- */
+
 public class TopologySideBar extends JPanel implements ActionListener
 {
     private final GUINetworkDesign callback;
     private final TopologyPanel topologyPanel;
     private final ITopologyCanvas canvas;
 
-    private final JToggleButton btn_multilayer;
     private final JToolBar layerToolBar;
     private final JButton btn_increaseInterLayerDistance, btn_decreaseInterLayerDistance;
     private final JButton btn_npChangeUndo, btn_npChangeRedo;
     private final JToggleButton btn_showLowerLayerInfo, btn_showUpperLayerInfo, btn_showThisLayerInfo;
-
-    private final MultiLayerControlPanel multilayerControlPanel;
-
-    private final JDialog dialog;
+    private final JPanel layersPanel;
 
     public TopologySideBar(GUINetworkDesign callback, TopologyPanel topologyPanel, ITopologyCanvas canvas)
     {
@@ -55,53 +73,6 @@ public class TopologySideBar extends JPanel implements ActionListener
         this.layerToolBar.setFloatable(false);
         this.layerToolBar.setOpaque(false);
 
-        this.multilayerControlPanel = new MultiLayerControlPanel(callback);
-        this.multilayerControlPanel.setBorder(BorderFactory.createTitledBorder("Layers"));
-
-        this.dialog = new JDialog();
-        this.dialog.setModal(false);
-        this.dialog.setType(JFrame.Type.UTILITY);
-        this.dialog.setLayout(new BorderLayout());
-        this.dialog.add(multilayerControlPanel);
-        this.dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        this.dialog.setResizable(false);
-        this.dialog.setUndecorated(true);
-        this.dialog.setAlwaysOnTop(true);
-        this.dialog.addComponentListener(new ComponentAdapter()
-        {
-            @Override
-            public void componentHidden(ComponentEvent componentEvent)
-            {
-                btn_multilayer.setSelected(false);
-            }
-        });
-        this.dialog.addMouseMotionListener(new MouseMotionListener() {
-            private int mx, my;
-
-            @Override
-            public void mouseMoved(MouseEvent e) {
-                mx = e.getXOnScreen();
-                my = e.getYOnScreen();
-            }
-
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                Point p = dialog.getLocation();
-                p.x += e.getXOnScreen() - mx;
-                p.y += e.getYOnScreen() - my;
-                mx = e.getXOnScreen();
-                my = e.getYOnScreen();
-                dialog.setLocation(p);
-            }
-        });
-        this.dialog.addWindowListener(new WindowAdapter()
-        {
-            @Override
-            public void windowClosed(WindowEvent windowEvent)
-            {
-                btn_multilayer.setSelected(false);
-            }
-        });
 
         /* Multilayer buttons */
         this.btn_increaseInterLayerDistance = new JButton();
@@ -121,8 +92,6 @@ public class TopologySideBar extends JPanel implements ActionListener
         this.btn_npChangeUndo.setToolTipText("Navigate back to the previous state of the network (last time the network design was changed)");
         this.btn_npChangeRedo = new JButton();
         this.btn_npChangeRedo.setToolTipText("Navigate forward to the next state of the network (when network design was changed");
-        this.btn_multilayer = new JToggleButton();
-        this.btn_multilayer.setToolTipText("Show layer control table");
 
         this.btn_increaseInterLayerDistance.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/increaseLayerDistance.png")));
         this.btn_decreaseInterLayerDistance.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/decreaseLayerDistance.png")));
@@ -131,7 +100,6 @@ public class TopologySideBar extends JPanel implements ActionListener
         this.btn_showLowerLayerInfo.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/showLayerLowerPropagation.png")));
         this.btn_npChangeUndo.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/undoButton.png")));
         this.btn_npChangeRedo.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/redoButton.png")));
-        this.btn_multilayer.setIcon(new ImageIcon(TopologyPanel.class.getResource("/resources/gui/showLayerControl.png")));
 
         this.btn_increaseInterLayerDistance.addActionListener(this);
         this.btn_decreaseInterLayerDistance.addActionListener(this);
@@ -140,17 +108,94 @@ public class TopologySideBar extends JPanel implements ActionListener
         this.btn_showThisLayerInfo.addActionListener(this);
         this.btn_npChangeUndo.addActionListener(this);
         this.btn_npChangeRedo.addActionListener(this);
-        this.btn_multilayer.addActionListener(this);
+        
+        /* Layers panel */
+        layersPanel = new JPanel(new MigLayout("insets 0 0 0 0, wrap 1"));
+        layersPanel.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
+        updateLayersPanel();
+        
+        final JScrollPane scPane = new JScrollPane(layersPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scPane.setLayout(new ScrollPaneLayout() {
+            @Override
+            public void layoutContainer(Container parent) {
+              JScrollPane scrollPane = (JScrollPane)parent;
 
-        this.layerToolBar.add(btn_multilayer);
+              Rectangle availR = scrollPane.getBounds();
+              availR.x = availR.y = 0;
+
+              Insets insets = parent.getInsets();
+              availR.x = insets.left;
+              availR.y = insets.top;
+              availR.width  -= insets.left + insets.right;
+              availR.height -= insets.top  + insets.bottom;
+
+              Rectangle vsbR = new Rectangle();
+              vsbR.width  = 8;
+              vsbR.height = availR.height;
+              vsbR.x = availR.x + availR.width - vsbR.width;
+              vsbR.y = availR.y;
+
+              if(viewport != null) {
+                viewport.setBounds(availR);
+              }
+              if(vsb != null) {
+                vsb.setVisible(true);
+                vsb.setBounds(vsbR);
+              }
+            }
+          });
+        scPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
+            private final Dimension d = new Dimension();
+            @Override protected JButton createDecreaseButton(int orientation) {
+              return new JButton() {
+                @Override public Dimension getPreferredSize() {
+                  return d;
+                }
+              };
+            }
+            @Override protected JButton createIncreaseButton(int orientation) {
+              return new JButton() {
+                @Override public Dimension getPreferredSize() {
+                  return d;
+                }
+              };
+            }
+            @Override
+            protected void paintTrack(Graphics g, JComponent c, Rectangle r) {}
+            @Override
+            protected void paintThumb(Graphics g, JComponent c, Rectangle r) {
+              Graphics2D g2 = (Graphics2D)g.create();
+              g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                                  RenderingHints.VALUE_ANTIALIAS_ON);
+              Color color = null;
+              JScrollBar sb = (JScrollBar)c;
+              if(!sb.isEnabled() || r.width>r.height)
+                return;
+              else
+                color = Color.LIGHT_GRAY;
+              
+              g2.setPaint(color);
+              g2.fillRoundRect(r.x,r.y,r.width,r.height,10,10);
+              g2.setPaint(Color.WHITE);
+              g2.drawRoundRect(r.x,r.y,r.width,r.height,10,10);
+              g2.dispose();
+            }
+            @Override
+            protected void setThumbBounds(int x, int y, int width, int height) {
+              super.setThumbBounds(x, y, width, height);
+              scrollbar.repaint();
+            }
+          });
+
         this.layerToolBar.add(btn_increaseInterLayerDistance);
         this.layerToolBar.add(btn_decreaseInterLayerDistance);
         this.layerToolBar.add(btn_showLowerLayerInfo);
         this.layerToolBar.add(btn_showUpperLayerInfo);
         this.layerToolBar.add(btn_showThisLayerInfo);
-        this.layerToolBar.add(Box.createVerticalGlue());
         //multiLayerToolbar.add(btn_npChangeUndo);
         //multiLayerToolbar.add(btn_npChangeRedo);
+        this.layerToolBar.add(scPane);
+        this.layerToolBar.add(Box.createVerticalGlue());
 
         this.add(layerToolBar, BorderLayout.WEST);
     }
@@ -204,22 +249,34 @@ public class TopologySideBar extends JPanel implements ActionListener
         } else if (src == btn_npChangeRedo)
         {
             callback.requestRedoAction();
-        } else if (src == btn_multilayer)
-        {
-            dialog.pack();
-            dialog.setVisible(btn_multilayer.isSelected());
-
-            dialog.validate();
-            dialog.repaint();
         }
     }
 
-    public void refresh()
+    public void updateLayersPanel()
     {
-        multilayerControlPanel.refreshTable();
-
-        dialog.pack();
-        dialog.validate();
-        dialog.repaint();
+    	layersPanel.removeAll();
+    	
+    	final NetPlan netPlan = callback.getDesign();
+    	
+    	for (NetworkLayer layer : netPlan.getNetworkLayers())
+    	{
+    		final String layerName = layer.getName().replaceAll(" ", "");
+    		final String buttonText = layerName.isEmpty() || layerName.contains("Layer") ? "LAY" + layer.getIndex() : (layerName.length() <= 4 ? layerName : layerName.substring(0, 4));
+			final JButton layerButton = new JButton();
+			layerButton.setOpaque(false);
+			layerButton.setContentAreaFilled(false);
+			layerButton.setBorderPainted(false);
+			layerButton.setFocusable(false);
+			layerButton.setMargin(new Insets(0, 0, 0, 0));
+	        layerButton.setText(buttonText.toUpperCase());
+    		layerButton.setToolTipText(layerName);
+    		layersPanel.add(layerButton);
+    		
+    	}
+    	
+    	layersPanel.revalidate();
+    	layersPanel.updateUI();
     }
+    
+    
 }
