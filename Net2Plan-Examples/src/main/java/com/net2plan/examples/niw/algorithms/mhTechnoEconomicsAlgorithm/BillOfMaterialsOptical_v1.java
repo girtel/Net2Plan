581fda32787f8bd6ee610efff7ef1d18c14697cf
ppavon <pablo.pavon@upct.es>
2020-02-10 17:05:21 +0100

    WIP - Javier take it from here

--- Net2Plan-Examples/src/main/java/com/net2plan/examples/niw/algorithms/mhTechnoEconomicsAlgorithm/BillOfMaterialsOptical_v1.java
+++ Net2Plan-Examples/src/main/java/com/net2plan/examples/niw/algorithms/mhTechnoEconomicsAlgorithm/BillOfMaterialsOptical_v1.java
@@ -1,3 +1,4 @@
+
 /* Problems: Islands are not linear (seldom they are). Many degree > 2 AMENs Some AMEN islands are huge (41 nodes) */
 
 /*******************************************************************************
@@ -6,16 +7,26 @@
  *******************************************************************************/
 
 
+
+// Sacar numeros para: fliterless sí/no, Topologia dense y small: fijar: CON tolerancia a fallos, trafico 2025, 
+// Ver % cost y % consumo, como se reparte entre: 
+// 1) Optica linea, optica transponder, IP, IT
+
+
+/*
+ * 
+ */
+
 package com.net2plan.examples.niw.algorithms.mhTechnoEconomicsAlgorithm;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 
-import com.net2plan.examples.niw.algorithms.mhTechnoEconomicsAlgorithm.TimExcel_NodeListSheet_forConfTimData.NODETYPE;
+import com.net2plan.examples.niw.algorithms.mhTechnoEconomicsAlgorithm.TecnoEc2_costModel.LineCards;
+import com.net2plan.examples.niw.algorithms.mhTechnoEconomicsAlgorithm.TecnoEc2_costModel.Pluggables;
+import com.net2plan.examples.niw.algorithms.mhTechnoEconomicsAlgorithm.TecnoEc2_costModel.RouterChassis;
 import com.net2plan.interfaces.networkDesign.Configuration;
 import com.net2plan.interfaces.networkDesign.IAlgorithm;
 import com.net2plan.interfaces.networkDesign.Net2PlanException;
@@ -23,39 +34,47 @@ import com.net2plan.interfaces.networkDesign.NetPlan;
 import com.net2plan.niw.OadmArchitecture_generic;
 import com.net2plan.niw.OadmArchitecture_generic.Parameters;
 import com.net2plan.niw.OpticalAmplifierInfo;
-import com.net2plan.niw.OpticalSpectrumManager;
 import com.net2plan.niw.WFiber;
 import com.net2plan.niw.WLightpath;
 import com.net2plan.niw.WNet;
 import com.net2plan.niw.WNode;
+import com.net2plan.niw.WServiceChain;
+import com.net2plan.niw.WVnfInstance;
 import com.net2plan.utils.InputParameter;
-import com.net2plan.utils.Pair;
 import com.net2plan.utils.Triple;
 
 
 
 public class BillOfMaterialsOptical_v1 implements IAlgorithm
 {
-    private enum OPTICALELEMENTS
+	private InputParameter trafficAcceptableByAServerGbps = new InputParameter("trafficAcceptableByAServerGbps", 40.0, "", 0, true, Double.MAX_VALUE, true);
+	private InputParameter maximumUtilizationOfAccessPorts = new InputParameter("maximumUtilizationOfAccessPorts", 0.5, "", 0, true, 1.0, true);
+
+	
+	private enum LAYERTYPE { OPTICAL , IT, IP};
+    private enum OPTICAL_IT_IP_ELEMENTS
     {
-    	TRANSPONDERBIDI_25G (0.2 , 0),
-    	TRANSPONDERBIDI_100G (0.2 , 0),
-    	SPLITTER1X2 (0.2 , 0),
-    	SPLITTER1X4 (0.2 , 0),
-    	SPLITTER1X8 (0.2 , 0),
-    	SPLITTER1XMORETHAN8 (0.2 , 0),
-    	EDFADOUBLEBOOSTERANDPREAMPL (0.2 , 0),
-    	WAVELENGTHBLOCKER (0.2 , 0),
-    	WSS_FIXED_1X4 (0.2 , 0),
-    	WSS_FIXED_1X9 (0.2 , 0),
-    	WSS_FIXED_1X20 (0.2 , 0),
-    	WSS_FLEXI_1X4 (0.2 , 0),
-    	WSS_FLEXI_1X9 (0.2 , 0),
-    	WSS_FLEXI_1X20 (0.2 , 0),
-    	MUXDEMUX80CHANNELS (0.2 , 0);
+    	ITSERVER (LAYERTYPE.IT , 25000 , 1000),
+    	TRANSPONDERBIDI_25G (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	TRANSPONDERBIDI_100G (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	SPLITTER1X2 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	SPLITTER1X4 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	SPLITTER1X8 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	SPLITTER1XMORETHAN8 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	EDFADOUBLEBOOSTERANDPREAMPL (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WAVELENGTHBLOCKER (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FIXED_1X4 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FIXED_1X9 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FIXED_1X20 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FLEXI_1X4 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FLEXI_1X9 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	WSS_FLEXI_1X20 (LAYERTYPE.OPTICAL , 0.2 , 0),
+    	MUXDEMUX80CHANNELS (LAYERTYPE.OPTICAL , 0.2 , 0);
     	private final double cost, consumption_W;
-		private OPTICALELEMENTS(double cost, double consumption_W) 
+    	private final LAYERTYPE layer;
+		private OPTICAL_IT_IP_ELEMENTS(LAYERTYPE layer , double cost, double consumption_W) 
 		{
+			this.layer = layer;
 			this.cost = cost;
 			this.consumption_W = consumption_W;
 		}
@@ -65,19 +84,19 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 		public double getConsumption_W() {
 			return consumption_W;
 		}
-		public static OPTICALELEMENTS getWssRequired (int numOutputsOrInputs)
+		public static OPTICAL_IT_IP_ELEMENTS getWssRequired (int numOutputsOrInputs)
 		{
 			if (numOutputsOrInputs <= 4) return WSS_FLEXI_1X4;
 			else if (numOutputsOrInputs <= 9) return WSS_FLEXI_1X9;
-			else if (numOutputsOrInputs <= 20) return OPTICALELEMENTS.WSS_FLEXI_1X20;
+			else if (numOutputsOrInputs <= 20) return OPTICAL_IT_IP_ELEMENTS.WSS_FLEXI_1X20;
 			throw new Net2PlanException ("Too large WSS. Not possible");
 		}
-		public static OPTICALELEMENTS getSplitterRequired (int numOutputsOrInputs)
+		public static OPTICAL_IT_IP_ELEMENTS getSplitterRequired (int numOutputsOrInputs)
 		{
 			if (numOutputsOrInputs <= 2) return SPLITTER1X2;
-			else if (numOutputsOrInputs <= 4) return OPTICALELEMENTS.SPLITTER1X4;
-			else if (numOutputsOrInputs <= 8) return OPTICALELEMENTS.SPLITTER1X8;
-			return OPTICALELEMENTS.SPLITTER1XMORETHAN8;
+			else if (numOutputsOrInputs <= 4) return OPTICAL_IT_IP_ELEMENTS.SPLITTER1X4;
+			else if (numOutputsOrInputs <= 8) return OPTICAL_IT_IP_ELEMENTS.SPLITTER1X8;
+			return OPTICAL_IT_IP_ELEMENTS.SPLITTER1XMORETHAN8;
 		}
     }
     
@@ -92,8 +111,8 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 		Configuration.setOption("precisionFactor", new Double(1e-9).toString());
 		Configuration.precisionFactor = 1e-9;
 		
-		final Map<WNode , Map<OPTICALELEMENTS,Double>> bomOptical_n = new HashMap<> ();
-		final Map<WFiber , Map<OPTICALELEMENTS,Double>> bomOptical_e = new HashMap<> ();
+		final Map<WNode , Map<OPTICAL_IT_IP_ELEMENTS,Double>> bomOptical_n = new HashMap<> ();
+		final Map<WFiber , Map<OPTICAL_IT_IP_ELEMENTS,Double>> bomOptical_e = new HashMap<> ();
 		
 		/* */
 		// 1. place preamplifiers and boosters: now simple: always booster and pre-amplifier, only line amplifier if 80 km
@@ -115,9 +134,9 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 		/* AMPLIFIERS: All nodes have preampl & booster, and one line amplifier for 80 km */
 		for (WFiber e : wNet.getFibers())
 		{
-			if (e.isExistingBoosterAmplifierAtOriginOadm()) increase (bomOptical_e.get(e) , OPTICALELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 0.5);
-			if (e.isExistingPreamplifierAtDestinationOadm()) increase (bomOptical_e.get(e) , OPTICALELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 0.5);
-			increase (bomOptical_e.get(e) , OPTICALELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , (double) e.getOpticalLineAmplifiersInfo().size());
+			if (e.isExistingBoosterAmplifierAtOriginOadm()) increase (bomOptical_e.get(e) , OPTICAL_IT_IP_ELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 0.5);
+			if (e.isExistingPreamplifierAtDestinationOadm()) increase (bomOptical_e.get(e) , OPTICAL_IT_IP_ELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 0.5);
+			increase (bomOptical_e.get(e) , OPTICAL_IT_IP_ELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , (double) e.getOpticalLineAmplifiersInfo().size());
 		}
 		
 		/* Directionless add/drop modules (the same if filterless or not): 1 splitter, 1 combiner, and an amplifier if more than 8 transponders 
@@ -126,10 +145,10 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 		for (WNode n : wNet.getNodes())
 		{
 			final int numTranspondersInTheNode = Math.max(n.getIncomingLigtpaths().size() , n.getOutgoingLigtpaths().size());
-			final OPTICALELEMENTS splitterRequired =  OPTICALELEMENTS.getSplitterRequired(numTranspondersInTheNode);
+			final OPTICAL_IT_IP_ELEMENTS splitterRequired =  OPTICAL_IT_IP_ELEMENTS.getSplitterRequired(numTranspondersInTheNode);
 			increase (bomOptical_n.get(n) , splitterRequired , 2.0); // one splitter one combiner
-			if (splitterRequired == OPTICALELEMENTS.SPLITTER1XMORETHAN8)
-				increase (bomOptical_n.get(n) , OPTICALELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 1.0); // extra amplification needed only if large splitter/combiner
+			if (splitterRequired == OPTICAL_IT_IP_ELEMENTS.SPLITTER1XMORETHAN8)
+				increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.EDFADOUBLEBOOSTERANDPREAMPL , 1.0); // extra amplification needed only if large splitter/combiner
 		}
 		
 		/* The OADM degrees, filterless or not */
@@ -144,16 +163,16 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 			for (WFiber out : n.getOutgoingFibers())
 			{
 				if (param.isBroadcastAndSelect() || param.isRouteAndSelect())
-					increase (bomOptical_n.get(n) , OPTICALELEMENTS.getWssRequired(numOutputsWssOrSplitter) , 1.0); 
+					increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.getWssRequired(numOutputsWssOrSplitter) , 1.0); 
 				else if (param.isFilterless())
-					increase (bomOptical_n.get(n) , OPTICALELEMENTS.getSplitterRequired(numOutputsWssOrSplitter) , 1.0); 
+					increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.getSplitterRequired(numOutputsWssOrSplitter) , 1.0); 
 			}
 			for (WFiber in : n.getIncomingFibers())
 			{
 				if (param.isRouteAndSelect())
-					increase (bomOptical_n.get(n) , OPTICALELEMENTS.getWssRequired(numOutputsWssOrSplitter) , 1.0); 
+					increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.getWssRequired(numOutputsWssOrSplitter) , 1.0); 
 				else if (param.isBroadcastAndSelect() || param.isFilterless())
-					increase (bomOptical_n.get(n) , OPTICALELEMENTS.getSplitterRequired(numOutputsWssOrSplitter) , 1.0); 
+					increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.getSplitterRequired(numOutputsWssOrSplitter) , 1.0); 
 			}
 		}
 		
@@ -161,18 +180,87 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
 		for (WLightpath lp : wNet.getLightpaths())
 		{
 			if (lp.getLightpathRequest().getLineRateGbps() == 25.0)
-				increase (bomOptical_n.get(lp.getA()) , OPTICALELEMENTS.TRANSPONDERBIDI_25G , 1.0);
+				increase (bomOptical_n.get(lp.getA()) , OPTICAL_IT_IP_ELEMENTS.TRANSPONDERBIDI_25G , 1.0);
 			else if (lp.getLightpathRequest().getLineRateGbps() == 100.0)
-				increase (bomOptical_n.get(lp.getA()) , OPTICALELEMENTS.TRANSPONDERBIDI_100G , 1.0);
+				increase (bomOptical_n.get(lp.getA()) , OPTICAL_IT_IP_ELEMENTS.TRANSPONDERBIDI_100G , 1.0);
 			else throw new Net2PlanException ("Unkonwn transponder line rate");
 		}
 			
+		
+		/* IT BOM */
+		if (wNet.getServiceChains().stream().anyMatch(sc->sc.getCurrentExpansionFactorApplied().stream().anyMatch(sf->sf != 1.0))) throw new Net2PlanException ("Some expansion factors are not 1.0");
+		for (WNode n : wNet.getNodes())
+		{
+			double trafficToOrFromTheServersGbps = 0;
+			for (WVnfInstance vnf : n.getVnfInstances())
+			{
+				for (WServiceChain sc : vnf.getTraversingServiceChains())
+				{
+					final double inTRafficThisSc = vnf.getOccupiedCapacityByTraversingRouteInGbps(sc);
+					final double outTRafficThisSc = inTRafficThisSc;
+					trafficToOrFromTheServersGbps += inTRafficThisSc;
+				}
+			}
+			increase (bomOptical_n.get(n) , OPTICAL_IT_IP_ELEMENTS.ITSERVER , trafficToOrFromTheServersGbps / trafficAcceptableByAServerGbps.getDouble());
+		}
+		
+		/* IP Scenario 1 */
+		if (wNet.getLightpathRequests().stream().anyMatch(lpr->lpr.getLineRateGbps() > 100.0)) throw new Net2PlanException ("Muxponders are needed for transponders of more than 100G");
+		for (WNode n : wNet.getNodes())
+		{
+			/* 10G ports coming from the access: OLTs, mobile, business, residential... */
+			final double trafGeneratedInTheNodeGbps = n.getOutgoingServiceChainRequests().stream().mapToDouble(scr->scr.getCurrentOfferedTrafficInGbps()).sum ();
+			final double trafFinishedInTheNodeGbps = n.getIncomingServiceChainRequests().stream().mapToDouble(scr->scr.getCurrentOfferedTrafficInGbps()).sum ();
+			final double accessTrafficInTheNodesGbps = Math.max(trafGeneratedInTheNodeGbps, trafFinishedInTheNodeGbps);
+			int num10GPortsAccessPlus10GLightpats = (int) Math.ceil(accessTrafficInTheNodesGbps / (10.0 * maximumUtilizationOfAccessPorts.getDouble()));
+			
+			/* 40G ports for the 25G transponders */
+			num10GPortsAccessPlus10GLightpats += (int) n.getOutgoingLigtpaths().stream().
+					filter(lp->lp.getLightpathRequest().getLineRateGbps() <= 10.0).count ();
+			final int num40GPortsTransponders = (int) n.getOutgoingLigtpaths().stream().
+					filter(lp->lp.getLightpathRequest().getLineRateGbps() > 10.0 && lp.getLightpathRequest().getLineRateGbps() <= 40.0).count ();
+			final int num100GPortsTransponders = (int) n.getOutgoingLigtpaths().stream().
+					filter(lp->lp.getLightpathRequest().getLineRateGbps() > 40.0 && lp.getLightpathRequest().getLineRateGbps() <= 100.0).count ();
+			
+			/* Compute the number of line cards */
+			final LineCards lc10G = TecnoEc2_costModel.lineCardsAvailable.stream().filter(lc->lc.getPortRateGbps() == 10.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final LineCards lc40G = TecnoEc2_costModel.lineCardsAvailable.stream().filter(lc->lc.getPortRateGbps() == 40.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final LineCards lc100G = TecnoEc2_costModel.lineCardsAvailable.stream().filter(lc->lc.getPortRateGbps() == 100.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final int numLc10G = (int) Math.ceil(num10GPortsAccessPlus10GLightpats / lc10G.getNumPorts());
+			final int numLc40G = (int) Math.ceil(num40GPortsTransponders / lc40G.getNumPorts());
+			final int numLc100G = (int) Math.ceil(num100GPortsTransponders / lc100G.getNumPorts());
+			final int totalNumLc = numLc10G + numLc40G + numLc100G; 
+			/* Choose the best architecture */
+			RouterChassis bestRc = null;
+			int bestNumChassis = -1;
+			double bestCost = Double.MAX_VALUE;
+			for (RouterChassis rc : TecnoEc2_costModel.routerChassisAvailable)
+			{
+				 int numChassis;
+				if (totalNumLc <= rc.getNumLineCards())
+					numChassis = 1; 
+				else if (totalNumLc <= 2 * (rc.getNumLineCards() - 1))
+					numChassis = 2;
+				else continue;
+				final int numExtraLcs = numChassis == 1? 0 : numChassis;
+				final double totalCost = numChassis * rc.getPriceChassisRspFanPowerSupplyFabricCardsNotLineCardsDollars() + numExtraLcs * lc100G.getPriceDollars();
+				if (totalCost < bestCost) { bestRc = rc; bestNumChassis = numChassis ; bestCost = totalCost; }
+			}
+			if (bestRc == null) throw new Net2PlanException ("No chassis options fits the requirements");
+			
+			/* */
+			final Pluggables plug10G = TecnoEc2_costModel.pluggablesAvailable.stream().filter(lc->lc.getLineRateGbps() == 10.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final Pluggables plug40G = TecnoEc2_costModel.pluggablesAvailable.stream().filter(lc->lc.getLineRateGbps() == 40.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final Pluggables plug100G = TecnoEc2_costModel.pluggablesAvailable.stream().filter(lc->lc.getLineRateGbps() == 100.0).sorted((e1,e2)->Double.compare(e1.getPriceDollars() , e2.getPriceDollars())).findFirst().get();
+			final int numPlugables10G = num10GPortsAccessPlus10GLightpats;
+			final int numPluggables40G = num40GPortsTransponders;
+			final int numPluggables100G = num100GPortsTransponders + (bestNumChassis == 1? 0 : bestNumChassis);
+		}
 		
 		/************************************************************************************************/
 		// Separate per equipment in line = everything but transponders vs transponders
 		// Separate cost between AMENs / MCENs / line amplifiers. Energy consumption differences?
 		// ...
-		
 		
 		return "Ok";
 	}
@@ -195,5 +283,7 @@ public class BillOfMaterialsOptical_v1 implements IAlgorithm
     	if (map.containsKey(key)) map.put(key, map.get(key) + amount); else map.put(key, amount);
     	return map;
     }
+
+    
     
 }
