dc1664d8ee8083736ede80ef0829d1b8f25796df
José Manuel Martínez Caro <jmmartinez@e-lighthouse.com>
2023-03-28 14:17:32 +0200

    first step n2p-api

---
+++ n2p-api/src/main/java/com/net2plan/api/N2pApiApplication.java
@@ -0,0 +1,13 @@
+package com.net2plan.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2pApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N2pApiApplication.class, args);
+	}
+
+}
---
+++ n2p-api/src/test/java/com/net2plan/api/N2pApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net2plan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N2pApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
