39d6265b0ed5f32e3daea778304fe1a6c39cd95b
Antoniogins <82613210+Antoniogins@users.noreply.github.com>
2023-09-12 13:07:46 +0200

    Fixed ClassLoading for JDK_9+
    Updated the class loading hierarchy from jdk8 to jdk9+.
    
    Net2Plan tested on JDK8 and JDK17. Fully functional.

--- Net2Plan-Core/src/main/java/com/net2plan/internal/SystemUtils.java
+++ Net2Plan-Core/src/main/java/com/net2plan/internal/SystemUtils.java
@@ -12,7 +12,7 @@
 
 
 
- 
+
 
 
 
@@ -52,32 +52,10 @@ public class SystemUtils
 	private static UserInterface ui = null;
 	private static Set<URL> defaultClasspath;
 
-	/**
-	 * Adds a given file to the classpath.
-	 * 
-	 * @param f File to be added
-	 * @since 0.3.1
-	 */
-	public static void addToClasspath(File f)
-	{
-		try
-		{
-			Method addURL = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
-			addURL.setAccessible(true);
-			
-			URL url = f.toURI().toURL();
-			ClassLoader cl = ClassLoader.getSystemClassLoader();
-			addURL.invoke(cl, new Object[] { url });
-		}
-		catch (NoSuchMethodException | SecurityException | MalformedURLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
-	
+
 	/**
 	 * Configures the environment for Net2Plan (locale, number format, look and feel...).
-	 * 
+	 *
 	 * @param mainClass Main class of the running program
 	 * @param ui User interface (GUI or CLI)
 	 * @since 0.2.3
@@ -94,7 +72,7 @@ public class SystemUtils
 
 		if (SystemUtils.ui != null) throw new RuntimeException("Environment was already configured");
 		SystemUtils.ui = ui;
-		
+
 		if (ui == UserInterface.GUI)
 		{
 			try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
@@ -149,9 +127,9 @@ public class SystemUtils
 			UIManager.put("Separator.shadow", bgColor);
 			UIManager.put("TitledBorder.titleColor", fgColor);
 		}
-		
+
 		defaultClasspath = getClasspath();
-		
+
 		/* Load options */
 		try { Configuration.readFromOptionsDefaultFile();  }
 		catch (IOException ex) { throw new Net2PlanException("Error loading options: " + ex.getMessage()); }
@@ -168,13 +146,58 @@ public class SystemUtils
 	 */
 	public static Set<URL> getClasspath()
 	{
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-		Set<URL> classpath = new TreeSet<URL>(new URLComparator());
-		classpath.addAll(Arrays.asList(((URLClassLoader) cl).getURLs()));
-		
-		return Collections.unmodifiableSet(classpath);
+		// Instead of casting ClassLoader to get the class paths, use
+		// system properties
+		String classPaths = System.getProperty("java.class.path");
+		String[] paths = classPaths.split(";");
+
+		// Create URLs of the classPaths
+		Set<URL> classPath = new TreeSet<URL>(new URLComparator());
+
+		try
+		{
+			for (String path : paths)
+				classPath.add( (new File(path) ).toURI().toURL() );
+		}
+		catch (MalformedURLException e)
+		{
+			throw new RuntimeException(e);
+		}
+
+		return Collections.unmodifiableSet(classPath);
 	}
-	
+
+	/**
+	 * Adds a given file to the classpath.
+	 *
+	 * @param f File to be added
+	 * @since 0.3.1
+	 */
+	public static void addToClasspath(File f)
+	{
+		try
+		{
+			Method addURL = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] { URL.class });
+			addURL.setAccessible(true);
+
+			URL url = f.toURI().toURL();
+
+			// New JDK-9+ ClassLoading hierarchy
+			// Invoke directly parent class loader
+//			ClassLoader cl = ClassLoader.getSystemClassLoader();
+			ClassLoader cl = SystemUtils.class.getClassLoader();
+			addURL.invoke(cl, new Object[] { url });
+		}
+		catch (NoSuchMethodException | SecurityException | MalformedURLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+
+
+
+
 	/**
 	 * Returns the current directory where the application is executing in.
 	 *
@@ -189,7 +212,7 @@ public class SystemUtils
 
 			File curDir = new File(mainClass.getProtectionDomain().getCodeSource().getLocation().toURI());
 			if (!curDir.isDirectory()) curDir = curDir.getAbsoluteFile().getParentFile();
-			
+
 			return curDir;
 		}
 		catch (RuntimeException | URISyntaxException e)
@@ -251,7 +274,7 @@ public class SystemUtils
 	{
 		return Files.getNameWithoutExtension(file.getAbsolutePath());
 	}
-	
+
 	/**
 	 * Returns the path of a file (without file name or extension).
 	 *
@@ -264,7 +287,7 @@ public class SystemUtils
 		String absolutePath = file.getAbsolutePath();
 		return absolutePath.substring(0, absolutePath.lastIndexOf(getDirectorySeparator()));
 	}
-	
+
 	/**
 	 * Returns the user classpath ({@code .class/.jar} files not added by default).
 	 *
@@ -276,7 +299,7 @@ public class SystemUtils
 		Set<URL> classpath = new TreeSet<URL>(new URLComparator());
 		classpath.addAll(getClasspath());
 		classpath.removeAll(defaultClasspath);
-		
+
 		return Collections.unmodifiableSet(classpath);
 	}
 
@@ -290,7 +313,7 @@ public class SystemUtils
 	{
 		return ui;
 	}
-	
+
 	private static class URLComparator implements Comparator<URL>
 	{
 		@Override
