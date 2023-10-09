0185c33b90ba840ee31af55923eaa610047e4f06
Antoniogins <82613210+Antoniogins@users.noreply.github.com>
2023-10-09 23:36:37 +0200

    Update Niw_AdvancedJTable_SegmentRouting.java
    More work on FlexAlgo information. Now it only shows up nodes that belong to SR network. Need further work.

--- Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/specificTables/Niw_AdvancedJTable_SegmentRouting.java
+++ Net2Plan-GUI/Net2Plan-GUI-Plugins/Net2Plan-NetworkDesign/src/main/java/com/net2plan/gui/plugins/networkDesign/viewEditTopolTables/controlTables/specificTables/Niw_AdvancedJTable_SegmentRouting.java
@@ -30,7 +30,9 @@ import com.net2plan.niw.WIpLink;
 import com.net2plan.niw.WNet;
 import com.net2plan.niw.WNode;
 import com.net2plan.utils.Pair;
+import com.net2plan.utils.Triple;
 import net.miginfocom.swing.MigLayout;
+import org.apache.commons.collections4.iterators.EntrySetMapIterator;
 
 import javax.swing.*;
 import java.awt.*;
@@ -92,13 +94,13 @@ public class Niw_AdvancedJTable_SegmentRouting extends AdvancedJTable_networkEle
         final NetworkLayer layer = this.getTableNetworkLayer();
 
 
-        assert callback.getNiwInfo().getFirst();
+//        assert callback.getNiwInfo().getFirst();
         final WNet wNet = callback.getNiwInfo().getSecond();
 
 
         final List<AjtRcMenu> res = new ArrayList<>();
 
-        res.add(new AjtRcMenu("Add FlexAlgo", e -> createFlexAlgoFromGUI(callback, layer) , (a, b) -> b>0, null));
+        res.add(new AjtRcMenu("Add FlexAlgo", e -> createFlexAlgoFromGUI(callback, layer), (a, b) -> true, null));
 
         // TODO the remaining actions
 
@@ -106,72 +108,66 @@ public class Niw_AdvancedJTable_SegmentRouting extends AdvancedJTable_networkEle
     }
 
 
-
-
-
     private static void createFlexAlgoFromGUI(final GUINetworkDesign callback, final NetworkLayer layer)
     {
+        /* Check for some first steps */
         final NetPlan netPlan = callback.getDesign();
         final WNet wNet = callback.getNiwInfo().getSecond();
-
+        if(!wNet.isSrInitialized()) wNet.initializeFlexAlgoAttributes();
 
 
         /* ID Selector */
-        JTextField idText = new JTextField();
-        JLabel idLabel = new JLabel("k of FlexAlgo. [128, 255]");
-        JPanel idPanel = new JPanel(new FlowLayout());
-        idPanel.add(idText);
-        idPanel.add(idLabel);
+        JTextField idText = new JTextField(6);
+        JLabel idLabel = new JLabel("Identifier (k) of FlexAlgo. [128, 255]");
 
         /* Calculation selector */
         List<Pair<String, Integer>> calculationTypes = WFlexAlgo.FlexAlgoProperties.getCalculationOptions();
-        JComboBox<String> calculationSelector = new WiderJComboBox<>((String[]) calculationTypes.stream().map(Pair::getFirst).toArray());
+        JComboBox<String> calculationSelector = new WiderJComboBox<>();
+        calculationTypes.forEach(pair -> calculationSelector.addItem(pair.getFirst()));
         JLabel calculationLabel = new JLabel("Calculation type");
-        JPanel calcPanel = new JPanel(new FlowLayout());
-        calcPanel.add(calculationSelector);
-        calcPanel.add(calculationLabel);
 
         /* Weight selector */
         List<Pair<String, Integer>> weightTypes = WFlexAlgo.FlexAlgoProperties.getWeightOptions();
-        JComboBox<String> weightSelector = new WiderJComboBox<>((String[]) weightTypes.stream().map(Pair::getFirst).toArray());
+        JComboBox<String> weightSelector = new WiderJComboBox<>();
+        weightTypes.forEach(pair -> weightSelector.addItem(pair.getFirst()));
         JLabel weightLabel = new JLabel("Calculation type");
-        JPanel weiPanel = new JPanel(new FlowLayout());
-        weiPanel.add(weightSelector);
-        weiPanel.add(weightLabel);
-
 
         /* Link selector */
-        JList<WIpLink> linkList = new JList( callback.getDesign().getLinks().stream().map(link -> toWIpLink.apply(Pair.of(link, wNet))).toArray() );
+        JList<Link> linkList = new JList(wNet.getIpLinks().toArray());
         linkList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         linkList.setLayoutOrientation(JList.VERTICAL);
         linkList.setVisibleRowCount(-1);
         JScrollPane linkListScroller = new JScrollPane(linkList);
-//        listScroller.setPreferredSize(new Dimension(250, 80));
-
+        linkListScroller.setPreferredSize(new Dimension(500, 160));
 
         /* Node selector */
-        JList<WNode> nodeList = new JList( callback.getDesign().getNodes().stream().map(toWNode).toArray() );
+//        JList<Node> nodeList = new JList(wNet.getNodes().toArray());
+        List<Triple<String, Long, String>> candidateNodeTuples = new ArrayList<>();
+        Map<String, Pair<Long, String>> candidateNodeMap = new TreeMap<>();
+        wNet.getNodes().stream().forEach(node -> {
+            node.getSidList().ifPresent( sidList -> sidList.forEach(sid -> candidateNodeMap.put("("+sid+") - "+node.getName(), Pair.of(node.getId(), sid)) ) );
+        });
+        JList<String> nodeList = new JList(candidateNodeMap.keySet().toArray());
+
         nodeList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         nodeList.setLayoutOrientation(JList.VERTICAL);
         nodeList.setVisibleRowCount(-1);
         JScrollPane nodeListScroller = new JScrollPane(nodeList);
-//        nodeListScroller.setPreferredSize(new Dimension(250, 80));
+        nodeListScroller.setPreferredSize(new Dimension(500, 160));
 
 
-        /* Scroll panels */
-        final JPanel selectionPanel = new JPanel(new FlowLayout());
-        selectionPanel.add(linkListScroller);
-        selectionPanel.add(nodeListScroller);
-
-
-
-        /* Create the layout to show all the elements */
-        final JPanel mainPanel = new JPanel();
-        mainPanel.add(idPanel);
-        mainPanel.add(calcPanel);
-        mainPanel.add(weiPanel);
-        mainPanel.add(selectionPanel);
-
+        /* Main panel construction */
+        JPanel mainPanel = new JPanel(new MigLayout("fill", "[][grow]", "[][][][][]"));
+        mainPanel.add(idLabel);
+        mainPanel.add(idText, "wrap");
+        mainPanel.add(calculationLabel);
+        mainPanel.add(calculationSelector, "wrap");
+        mainPanel.add(weightLabel);
+        mainPanel.add(weightSelector, "wrap");
+        mainPanel.add(new JLabel("Nodes (optional"));
+        mainPanel.add(new JLabel("Links (optional)"), "wrap");
+        mainPanel.add(nodeListScroller);
+        mainPanel.add(linkListScroller);
 
 
 
@@ -179,7 +175,8 @@ public class Niw_AdvancedJTable_SegmentRouting extends AdvancedJTable_networkEle
         /* Events */
 
         // Listener for identifier checker. ID must belong to [128, 255] and not be already in use
-        idText.addActionListener(new ActionListener() {
+        idText.addActionListener(new ActionListener()
+        {
             @Override
             public void actionPerformed(ActionEvent e)
             {
@@ -189,37 +186,101 @@ public class Niw_AdvancedJTable_SegmentRouting extends AdvancedJTable_networkEle
                     int id = Integer.parseInt(text);
 
                     // Check range
-                    if(id < 128 || id > 255) throw new NumberFormatException();
+                    if (id < 128 || id > 255) throw new NumberFormatException();
+
                     // Check that it is not already in use
-                    wNet.performOperationOnFlexAlgoRepository( repo -> {
-                        if(repo.containsKey(id)) throw new NumberFormatException();
+                    wNet.performOperationOnFlexAlgoRepository(repo -> {
+                        if (repo.containsKey(id)) throw new NumberFormatException();
                     });
 
                     // If correct
                     idText.setBorder(BorderFactory.createLineBorder(Color.GREEN));
-                } catch (NumberFormatException nfe)
-                {
-                    idText.setBorder(BorderFactory.createLineBorder(Color.RED));
-                }
-
-
-
+                } catch (NumberFormatException nfe) { idText.setBorder(BorderFactory.createLineBorder(Color.RED)); }
             }
         });
+
+
+        // Selection panels
+        ListSelectionModel nodeSelectionModel = new DefaultListSelectionModel()
+        {
+            boolean gestureStarted = false;
+            @Override
+            public void setSelectionInterval(int index0, int index1)
+            {
+                if (!gestureStarted)
+                {
+                    if (isSelectedIndex(index0)) super.removeSelectionInterval(index0, index1);
+                    else super.addSelectionInterval(index0, index1);
+                }
+                gestureStarted = true;
+            }
+
+            @Override
+            public void setValueIsAdjusting(boolean isAdjusting) { if (!isAdjusting) gestureStarted = false; }
+        }; nodeList.setSelectionModel(nodeSelectionModel);
+
+        ListSelectionModel linkSelectionModel = new DefaultListSelectionModel()
+        {
+            boolean gestureStarted = false;
+            @Override
+            public void setSelectionInterval(int index0, int index1)
+            {
+                if (!gestureStarted)
+                {
+                    if (isSelectedIndex(index0)) super.removeSelectionInterval(index0, index1);
+                    else super.addSelectionInterval(index0, index1);
+                }
+                gestureStarted = true;
+            }
+
+            @Override
+            public void setValueIsAdjusting(boolean isAdjusting) { if (!isAdjusting) gestureStarted = false; }
+        }; linkList.setSelectionModel(linkSelectionModel);
+
 
 
 
         // Launch the confirm dialog and wait to fill information. When information recovered, do all the stuff
         while (true)
         {
+            /* Launch the dialog */
             int result = JOptionPane.showConfirmDialog(null, mainPanel, "Please enter information for the new flex algo", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
             if (result != JOptionPane.OK_OPTION) return;
 
-            // Recover information from all tables TODO
 
-            // Create the FlexAlgoProperties
+            try
+            {
+                /* Recover information from all tables */ //TODO
+                int k = Integer.parseInt(idText.getText());
+                int calculation = calculationTypes.stream().filter(pair -> pair.getFirst().equals(calculationSelector.getSelectedItem()) ).findFirst().get().getSecond();
+                int weight = weightTypes.stream().filter(pair -> pair.getFirst().equals(weightSelector.getSelectedItem())).findFirst().get().getSecond();
 
-            // Perform operation on the flex algo repository -> add the flex algo property
+                List<Link> links = linkList.getSelectedValuesList();
+                Set<Long> linkIdList = links.stream().map(Link::getId).collect(Collectors.toSet());
+
+                List<String> nodeRepresentatives = nodeList.getSelectedValuesList();
+                List<Pair<Long, String>> selectedNodesInformation = nodeRepresentatives.stream().map(candidateNodeMap::get).collect(Collectors.toList());
+                Set<String> flexNodes = selectedNodesInformation.stream().map(Pair::getSecond).collect(Collectors.toSet());
+
+                
+
+                // TODO check if k is already in use
+
+                /* Create the FlexAlgoProperties */
+                WFlexAlgo.FlexAlgoProperties wflex = new WFlexAlgo.FlexAlgoProperties(k, calculation, weight, Optional.of(linkIdList), Optional.of(flexNodes));
+                wNet.performOperationOnFlexAlgoRepository(repo -> repo.mapFlexAlgoId2FlexAlgoProperties.put(k, wflex));
+
+                /* Perform operation on the flex algo repository -> add the flex algo property */
+
+                /* Perform operation on nodes (set this flex algo as used), links, etc */
+
+
+
+            } catch (Exception e) { continue; }
+
+
+
+
 
 
 
